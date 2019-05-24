141a2da2e8a8f5a925ad6872942e286d0a1969c8
Al Pa Ho <petrov51400@gmail.com>
2019-05-24 13:07:57 +0300

    Add ICell+Cell, but not implement

---
+++ ElementaryTasks/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementaryTasks
+{
+    class Cell : ICell
+    {
+        public int Heigth { get; private set; }
+        public int Width { get; private set; }
+        public bool IsDark { get; private set; }
+
+        public Cell(int heigth, int width, bool isDark)
+        {
+            this.Heigth = heigth;
+            this.Width = width;
+            this.IsDark = isDark;
+        }
+
+    }
+}
---
+++ ElementaryTasks/ICell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementaryTasks
+{
+    interface ICell
+    {
+        bool IsDark { get; }
+        int Heigth { get; }
+        int Width { get; }
+        //Point gorizontalX
+        //Point verticalY
+    }
+}
